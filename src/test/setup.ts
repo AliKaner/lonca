2b782e8b16bc081d67a179313459edec8b268785
Ali Kaner <alikaner.dev@gmail.com>
2026-03-05 00:33:43 +0300

    boom!!

--- src/test/setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-
-// global cleanup after each test
-afterEach(() => {
-  cleanup();
-});
